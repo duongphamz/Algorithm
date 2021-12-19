f0d139bf1bf194ef42a07e6c70958e1e44ab22a9
judares <pn.kyduong95@gmail.com>
2021-12-19 22:59:42 +0700

    Create deviceNameSystem.swift

---
+++ Hackerrank/deviceNameSystem.swift
@@ -0,0 +1,26 @@
+//input: ["mixer", "toaster", "mixer", "tv"] => output: ["mixer", "toaster", "mixer1", "tv"]
+
+func deviceNamesSystem(devicenames: [String]) -> [String] {
+    var result = [String]()
+    let deviceNameSet = Set(devicenames)
+    var occurArray: [String: Int] = [:]
+    
+    for name in deviceNameSet {
+        occurArray[name] = 0
+    }
+    
+    for name in devicenames {
+        var resultName = name
+        
+        if occurArray[name] == 0 {
+            resultName = name
+        } else {
+            resultName = name + "\(String(occurArray[name]!))"
+        }
+        occurArray[name]! += 1
+        
+        
+        result.append(resultName)
+    }
+    return result
+}
