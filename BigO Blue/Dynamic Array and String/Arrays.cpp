d495091a5780f7dd6e71499dc8a2a1ce3a16c4b5
judares <pn.kyduong95@gmail.com>
2022-01-01 09:49:37 +0700

    Create Arrays.cpp

---
+++ BigO Blue/Dynamic Array and String/Arrays.cpp
@@ -0,0 +1,36 @@
+// Arrays.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+Codeforces
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nA, nB;
+    int k, m;
+
+    cin >> nA >> nB;
+    cin >> k >> m;
+    int maxNumberOfArrayA = 0;
+    int minNumberOfArrayB = 0;
+
+    for (int i = 0; i < nA; i++) {
+        int number;
+        cin >> number;
+        if (i + 1 == k) {
+            maxNumberOfArrayA = number;
+        }
+    }
+
+    for (int j = 0; j < nB; j++) {
+        int number;
+        cin >> number;
+        if (j == nB - m) {
+            minNumberOfArrayB = number;
+        }
+    }
+
+    cout << (maxNumberOfArrayA < minNumberOfArrayB ? "YES" : "NO");
+}
+
