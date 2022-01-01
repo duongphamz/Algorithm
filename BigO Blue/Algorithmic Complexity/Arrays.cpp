464bcd029e611aa26ba17410ac4b39ba7ac95209
judares <pn.kyduong95@gmail.com>
2022-01-01 09:51:28 +0700

    Create Arrays.cpp

---
+++ BigO Blue/Algorithmic Complexity/Arrays.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+const int MAX = 1e5 + 5;
+https://codeforces.com/problemset/problem/224/B
+int fre[MAX];
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n + 1);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int unique = 0;
+
+    for (int i = 0; i < n; i++) {
+        fre[a[i]]++;
+
+        if (fre[a[i]] == 1) {
+            unique++;
+        }
+
+        if (unique == k) {
+            for (int j = 0; j < i + 1; j++) {
+                fre[a[j]]--;
+                if (fre[a[j]] == 0) {
+                    cout << j + 1 << " " << i + 1;
+                    return 0;
+                }
+            }
+        }
+    }
+
+    cout << "-1 -1" << endl;
+    return 0;
+}
