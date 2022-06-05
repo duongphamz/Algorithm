9eeb3b2fa8ef96298b2120dc73e04f02106b6ff9
duongphamz <pn.kyduong95@gmail.com>
2022-06-05 10:56:53 +0700

    Update 9. Palindrome Number.swift

--- Leetcode/9. Palindrome Number.swift
+++ Leetcode/9. Palindrome Number.swift
@@ -18,3 +18,19 @@ func isPalindrome(_ x: Int) -> Bool {
     
     return true
 }
+
+//Python solution
+// class Solution:
+//     def isPalindrome(self, x: int) -> bool:
+//         if x < 0:
+//             return False
+
+//         reversedNumber = 0
+//         originalNumber = x
+
+//         while x != 0:
+//             _remainder = x % 10
+//             reversedNumber = reversedNumber * 10 + _remainder
+//             x //= 10
+
+//         return reversedNumber == originalNumber
